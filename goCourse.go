c99cf153488bce57500665f0f3c1b0cb78bb34d6
evegcar <carlos.vega.garcia@ericsson.com>
2023-06-04 14:25:01 +0200

    Add multiples function

--- goCourse.go
+++ goCourse.go
@@ -5,9 +5,15 @@ import "fmt"
 
 func main() {
 	// Sign function
+	fmt.Printf("1. Sign function results:\n")
 	printSign(-5)
 	printSign(5)
 	printSign(0)
+
+	// sumMul function
+	fmt.Printf("\n2. Multiples function results:\n")
+	max := 3000000
+	fmt.Printf("The sum of multiplies of 3 and 5, but not of 15, from 0 to %d is %d\n", max, sumMul3_5_no15(max))
 }
 
 // sign function returns the sign of the argument number. -1 for negatives, 0 for zero, 1 for positives
@@ -27,4 +33,16 @@ func sign(n int) int {
 func printSign(n int) {
 	sign := sign(n)
 	fmt.Printf("The sign of %d is %d\n", n, sign)
+}
+
+// sumMul3_5_no15 returns the sum of multiple numbers of 3 and 5 but not for 15 until a max number
+// Enunciado: Considera la serie aritmética entre 0 y 3.000.000. Calcula la suma de todos los múltiplos de 3 (0, 3, 6, 9, 12, 15, ...) y de 5 (0, 5, 10, 15 ... ) pero que no sean múltiplos de 15
+func sumMul3_5_no15(max int) int {
+	sum := 0
+	for i := 0; i <= max; i++ {
+		if (i%3 == 0 || i%5 == 0) && (i%15 != 0) {
+			sum = sum + i
+		}
+	}
+	return sum
 }
